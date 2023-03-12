15f56184ceb36fce5c467c8d192878344d276f6f
Berker Gungor <yasarberkergungor@gmail.com>
2023-03-12 12:31:25 +0300

    feat: add solution to exercise 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -11,6 +11,7 @@ import {
 } from '../utils'
 
 const AppStateContext = React.createContext()
+const AppUpdaterContext = React.createContext()
 
 const initialGrid = Array.from({length: 100}, () =>
   Array.from({length: 100}, () => Math.random() * 100),
@@ -38,11 +39,11 @@ function AppProvider({children}) {
     dogName: '',
     grid: initialGrid,
   })
-  // 🐨 memoize this value with React.useMemo
-  const value = [state, dispatch]
   return (
-    <AppStateContext.Provider value={value}>
-      {children}
+    <AppStateContext.Provider value={state}>
+      <AppUpdaterContext.Provider value={dispatch}>
+        {children}
+      </AppUpdaterContext.Provider>
     </AppStateContext.Provider>
   )
 }
@@ -55,8 +56,16 @@ function useAppState() {
   return context
 }
 
+function useAppUpdater() {
+  const context = React.useContext(AppUpdaterContext)
+  if (!context) {
+    throw new Error('useAppUpdater must be used within the AppProvider')
+  }
+  return context
+}
+
 function Grid() {
-  const [, dispatch] = useAppState()
+  const dispatch = useAppUpdater()
   const [rows, setRows] = useDebouncedState(50)
   const [columns, setColumns] = useDebouncedState(50)
   const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
@@ -74,7 +83,8 @@ function Grid() {
 Grid = React.memo(Grid)
 
 function Cell({row, column}) {
-  const [state, dispatch] = useAppState()
+  const state = useAppState()
+  const dispatch = useAppUpdater()
   const cell = state.grid[row][column]
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -93,7 +103,8 @@ function Cell({row, column}) {
 Cell = React.memo(Cell)
 
 function DogNameInput() {
-  const [state, dispatch] = useAppState()
+  const state = useAppState()
+  const dispatch = useAppUpdater()
   const {dogName} = state
 
   function handleChange(event) {
